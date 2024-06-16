f7aab8490019a629970c5ba62f86a286fda4c3ba
deep-coder1 <ndktbd@gmail.com>
2024-06-16 22:23:54 +0530

    One Lesson Complete etc.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 30dc-js-essentials
 30dc-js-essentials
+
+<!-- Time Pause 10:35 -->
---
+++ one.js
@@ -0,0 +1,9 @@
+//This is single Line Comment
+/* This 
+is 
+multi
+Line
+Comment
+*/
+console.log("Hello World");
+console.log("19+12");
