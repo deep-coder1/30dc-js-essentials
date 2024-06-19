95e8735ec668a3eefe9cccd2e2fc478d46a70d59
deep-coder1 <ndktbd@gmail.com>
2024-06-19 06:40:36 +0530

    Data Types Lesson Three Working etc.

---
+++ three.js
@@ -0,0 +1,12 @@
+// Data Types
+
+// Primitive
+// Non Primitive
+
+let num1 = "50";
+let num2 = 50;
+console.log(typeof num1);
+console.log(typeof num2);
+
+let str = "Hello";
+console.log(typeof str);
