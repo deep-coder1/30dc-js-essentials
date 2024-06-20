304710f034b226caaf6b045bac86d82df48cfa64
deep-coder1 <ndktbd@gmail.com>
2024-06-20 19:37:29 +0530

    Data Types Lesson Three Complete etc.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 30dc-js-essentials
 
 <!-- Lesson 2 Time Pause 15:45 -->
-<!-- Lesson (03). 15:45 / 17:21, (01:06:29) Vid (27) Time End -->
+<!-- Lesson (03). 19:50 / 17:21, (01:06:29) Vid (27) Time End -->
--- three.js
+++ three.js
@@ -3,6 +3,9 @@
 // Primitive
 // Non Primitive
 
+
+// Primitive Data Type
+
 let num1 = "50";
 let num2 = 50;
 console.log(typeof num1);
@@ -10,3 +13,16 @@ console.log(typeof num2);
 
 let str = "Hello";
 console.log(typeof str);
+
+let bool = false;
+console.log(typeof bool);
+
+let a = null;
+console.log(typeof a)
+
+let b = undefined;
+console.log(typeof b);
+let c;
+console.log(b);
+
+
