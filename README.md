bb2afa37632c81264dea4ad4eb8dba1e0128fa6a
deep-coder1 <ndktbd@gmail.com>
2024-06-21 21:36:49 +0530

    Operators Lesson Four Working etc.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 <!-- Lesson 2 Time Pause 15:45 -->
 <!-- Lesson (03). 19:50 / 17:21, (01:06:29) Vid (27) Time End -->
+<!-- Lesson (04) Operators. 19:50 / 21:58, (01:06:29) Vid (27) Time End -->
---
+++ four.js
@@ -0,0 +1,12 @@
+// Operator
+
+let a = 10;
+let b = 5;
+
+// Arithemetic Operator
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(11 % b);
+console.log(a ** b);
