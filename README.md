d1f21b5da11de8d016315faafea994750d45388a
deep-coder1 <ndktbd@gmail.com>
2024-06-22 21:54:27 +0530

    Operators Type Conversion Lesson Four Working etc.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 30dc-js-essentials
 
 <!-- Lesson 2 Time Pause 15:45 -->
-<!-- Lesson (03). 19:50 / 17:21, (01:06:29) Vid (27) Time End -->
-<!-- Lesson (04) Operators. 19:50 / 21:58, (01:06:29) Vid (27) Time End -->
+<!-- Lesson (03). 19:50 / 17:21, (01:06:29) Vid Time End -->
+<!-- Lesson (04) Operators. 21:58 / 28:05, (01:06:29) Vid Time End -->
+<!-- Type Conversion -->
--- four.js
+++ four.js
@@ -1,12 +1,33 @@
 // Operator
 
-let a = 10;
-let b = 5;
+// let a = 10;
+// let b = 5;
 
 // Arithemetic Operator
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(11 % b);
-console.log(a ** b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(11 % b);
+// console.log(a ** b);
+
+// let num = 1;
+// num++;
+// console.log(num++);
+// console.log(++num);
+// console.log(num);
+
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+let num1 = 10;
+let num2 = "10";
+
+console.log(num1 == num2);  // True
+console.log(num1 === num2);  // False
+console.log(num1 !== num2);  // True
+
